91dbd48882ee8dd5da2d4aa94ef18296e51a9c52
Jimmy Fellous <jimmyfellous@gmail.com>
2025-08-26 01:02:35 +0200

    Mettre '2024' à la même taille que le sous-titre (0.93rem)

--- src/app/la-serre-aux-papillons/page.tsx
+++ src/app/la-serre-aux-papillons/page.tsx
@@ -86,7 +86,7 @@ export default function LaSerrePage() {
               role : composer & sound designer
             </p>
             
-            <p style={{ marginBottom: '15px', fontWeight: '300' }}>
+            <p style={{ marginBottom: '15px', fontWeight: '300', fontSize: '0.93rem' }}>
               2024
             </p>
             
